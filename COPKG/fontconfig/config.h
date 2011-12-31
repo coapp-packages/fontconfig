fd3bfdbdc9ca9261c8cf9a3479c3f7e07892bdfb
Vincent Povirk <madewokherd@gmail.com>
2011-12-30 19:14:39 -0500

    Add VC10 solution, building fc-case.

---
+++ COPKG/fontconfig/config.h
@@ -0,0 +1,15 @@
+#define ENABLE_LIBXML2 1
+
+#undef HAVE_GETOPT
+
+#undef HAVE_GETOPT_LONG
+
+#undef HAVE_INTTYPES_H
+
+#undef HAVE_LINK
+
+#undef HAVE_MMAP
+
+#define HAVE_STDINT_H 1
+
+#undef HAVE_XMLPARSE_H
